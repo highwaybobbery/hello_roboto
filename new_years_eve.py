277c12a8dee5496341d0cb28e7c3aa321e184e34
Margaret Berry <codeberry1@gmail.com>
2017-12-30 20:26:44 -0800

    write star wars theme

--- new_years_eve.py
+++ new_years_eve.py
@@ -7,8 +7,40 @@ BuzzerPin = 10    # pin10
 
 SPEED = 1
 
-# Song is a list of tones with name and 1/duration. 16 means 1/16
-SONG =	[
+# Song is a list of tones with name and 1/duration. 16 means 1/16th note
+# 16 = sixteenth note
+# 12 = triplet
+# 8 = eighth note
+# 4 = quarter note
+# 2 = half note
+# 1 = whole note
+# p =rest
+
+FUR_ELISE =	[
+	["a4",4],["e5",16],["eb5",16],
+	["e5",16],["eb5",16],["e5",16],["b4",16],["d5",16],["c5",16],
+	["a4",8],["p",16],["c4",16],["e4",16],["a4",16],
+	["b4",8],["p",16],["e4",16],["ab4",16],["b4",16],
+	["c5",8],["p",16],["e4",16],["e5",16],["eb5",16],
+	["e5",16],["eb5",16],["e5",16],["b4",16],["d5",16],["c5",16],
+	["a4",8],["p",16],["c4",16],["e4",16],["a4",16],
+	["b4",8],["p",16],["e4",16],["c5",16],["b4",16],["a4",4]
+	]
+
+#4/4 time
+STAR_WARS =	[
+	["p",4], ["p",4], ["p",4], ["d4",12],["d4",12],["d4",12],
+	["g4",2], ["d5",2],
+	["c5",12],["b4",12],["a4",12], ["g5",2], ["d5",4],
+	["c5",12],["b4",12],["a4",12], ["g5",2], ["d5",4],
+	["c5",12],["b4",12],["c5",12], ["a4",2], ["d4",12],["d4",12],["d4",12],
+	["g4",2], ["d5",2],
+	["c5",12],["b4",12],["a4",12], ["g5",2], ["d5",4],
+	["c5",12],["b4",12],["a4",12], ["g5",2], ["d5",4],
+	["c5",12],["b4",12],["c5",12], ["a4",2], ["p",4]
+	]
+
+GAME_OF_THRONES =	[
 	["a4",4],["e5",16],["eb5",16],
 	["e5",16],["eb5",16],["e5",16],["b4",16],["d5",16],["c5",16],
 	["a4",8],["p",16],["c4",16],["e4",16],["a4",16],
@@ -35,7 +67,7 @@ def play_note(player, frequency, duration):
 		player.stop()
 
 def run():
-	music = map(frequency_and_duration, SONG)
+	music = map(frequency_and_duration, STAR_WARS)
 	first_note_played = False
 	player = False
 
@@ -45,10 +77,10 @@ def run():
 			play_note(player, note['frequency'], note['duration'])
 		else:
 			print("playing first note")
-			first_note_played = True			
+			first_note_played = True
 			player = GPIO.PWM(BuzzerPin, note['frequency'])
 			player.start(note['duration'])
-			
+
 			time.sleep(note['duration'])
 
 
